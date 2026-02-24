7e06e2b132abc33067e03286262b83f1592f9893
Erik_Eike_Silva <erikeikesilva@gmail.com>
2026-02-24 15:58:44 -0300

    Delete 🚀 Guia React: Do Web ao Mobile directory

--- 🚀 Guia React: Do Web ao Mobile/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'  // Mantém apenas o index.css, que importa o App.css indiretamente
-// ou import './App.css' se preferir usar apenas App.css
